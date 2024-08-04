be24c06e27f8357001f004f3fac4209358dfc0b1
lfybtx <1278560674@qq.com>
2024-08-04 20:34:18 +0800

    新建项目

---
+++ src/main/java/com/lf/smallvote/SmallvoteApplication.java
@@ -0,0 +1,13 @@
+package com.lf.smallvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallvoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallvoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lf/smallvote/SmallvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lf.smallvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallvoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
